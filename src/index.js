6d0109b7d3075aa63134e5d0743546cf13641d82
Wojciech Migdal <wojciechjanmigdal@gmail.com>
2023-07-07 14:55:41 +0200

    adding redux, step 1

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GlobalStyle } from './GlobalStyle';
 import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux';
 import { theme } from './theme';
+import store from './store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { ConfigureStore, configureStore } from "@reduxjs/toolkit";
+import tasksReducer from './features/tasks/tasksSlice';
+
+export default configureStore({
+    reducer: {
+        tasks: tasksReducer,
+    },
+});
