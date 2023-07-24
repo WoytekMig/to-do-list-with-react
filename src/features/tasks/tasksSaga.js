4fbcac0ad2a4a84697b92def513ed7550a96db60
Wojciech Migdal <wojciechjanmigdal@gmail.com>
2023-07-24 15:29:18 +0200

    added localStorage to tasksSaga

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,6 +1,7 @@
-import { takeEvery, call, put, takeLatest, delay } from "redux-saga/effects";
-import { fetchExampleTasks, setTasks } from "./tasksSlice";
+import { takeEvery, call, put, takeLatest, delay, select } from "redux-saga/effects";
+import { fetchExampleTasks, selectTasks, setTasks } from "./tasksSlice";
 import { getExampleTasks } from './getExampleTasks';
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
 
 function* fetchExampleTasksHandler() {
     try {
@@ -12,7 +13,13 @@ function* fetchExampleTasksHandler() {
     }
 }
 
-export function* watchFetchExampleTasks() {
+function* saveTasksInLocalStorageHandler() {
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocalStorage  , tasks);
+}
+
+export function* tasksSaga() {
     console.log("Saga wpięta");
     yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+    yield takeEvery("*", saveTasksInLocalStorageHandler);
 }
