bb884b44efbd907dce4df5a85e71bf3cd2d68518
Wojciech Migdal <wojciechjanmigdal@gmail.com>
2023-08-02 13:01:53 +0200

    added emoji in authorPage

--- src/features/AuthorPage/AuthorPage.js
+++ src/features/AuthorPage/AuthorPage.js
@@ -9,13 +9,16 @@ function AuthorPage() {
       <Header title="About Author" />
       <Section title=" Wojtek Migdał "
         body={
-          <>Hey ! My name is Wojtek, and I'm 34 ;)
-            In this, let's just say 'interesting times for IT', I'm giving myself a chance :D
-            In my free time, I love to play: 1. with my kids, 2. footbal 3. some old RPGs.. (in that order!:D )
+          <>Hey ! My name is Wojtek, and I'm 34 &#128512;
+            In this, let's just say 'interesting time for IT', I'm giving myself a chance &#128516;
+            <p>In my free time, I love to play:</p> <ul>1. with my kids&#129490;,</ul> <ul>2. footbal&#9917;</ul> 
+            <ul>3. some old RPGs..&#128050;</ul> 
+           <ul><i>(in that order!&#128518; )</i> </ul>
             <p>When I have an oportunity to see <strong>Arsenal</strong> match - 
-            well... I'm not turning my eyes off :P</p>
-            <p>I'm a great <strong>Bon Jovi</strong> fan too..
-              They have great music for my every kind of mood.. Love them! <strong> : )</strong> </p>
+            well... I'm not turning my eyes off &#128514; </p>
+            <p>I'm a great &#127925; <strong>Bon Jovi</strong> &#127925; fan too..
+              They have great music for my every kind of mood..  </p>
+              <p>Just love them! &#129505;</p>
           </>
         } />
     </Container>
